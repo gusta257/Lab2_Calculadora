64c4bf33590938e2e6236c945bb5afceddfea0db
LCEST <lcesturban10@gmail.com.com>
2018-01-30 18:33:07 -0600

    Leer Archivo 1.2

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -17,19 +17,24 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Bienvenido");
+        
     try {
-        FileReader fr = new FileReader("C:\\Users\\Luis\\Desktop\\Prueba\\dato.txt");
+        FileReader fr = new FileReader("C:\\Users\\Luis\\Desktop\\Prueba\\datos.txt");
         BufferedReader bf = new BufferedReader(fr);
         String sCadena;
   
         while ((sCadena = bf.readLine())!=null) {
         System.out.println(sCadena);
+        //sCadena = bf.readLine();
+        //System.out.println(sCadena);
  } 
 
 } catch(Exception e)
         {
             System.out.println("valio");
 }
+     System.out.println("Gracias por esperar");
 }
     }
 
