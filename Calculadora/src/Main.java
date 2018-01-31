9f36a8cdd31c64e2313cf1928957ec4dfa4c501c
LCEST <lcesturban10@gmail.com.com>
2018-01-30 18:16:30 -0600

    Leer Archivo

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -3,18 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.*;
 /**
  *
  * @author Gustavo
+ * @author Luis Esturban
  */
 public class Main {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+    try {
+        FileReader fr = new FileReader("C:\\Users\\Luis\\Desktop\\Prueba\\dato.txt");
+        BufferedReader bf = new BufferedReader(fr);
+        String sCadena;
+  
+        while ((sCadena = bf.readLine())!=null) {
+        System.out.println(sCadena);
+ } 
+
+} catch(Exception e)
+        {
+            System.out.println("valio");
 }
+}
+    }
+
